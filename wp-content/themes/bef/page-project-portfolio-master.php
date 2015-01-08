be2ec9ed3f7e1fd74a88aab02fa08f99e1f31f62
unknown <lars@goodchee.com>
2015-01-08 11:37:39 -0800

    project portfolio updates

---
+++ wp-content/themes/bef/page-project-portfolio-master.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Template Name: Page - Project Portfolio Master
+*/
+?>
+<?php get_header('solutions'); ?>
+
+<?php get_template_part('partials/breadcrumbs'); ?>
+
+
+<div class="four columns subnav">
+	
+	<h4>Projects and Programs</h4>
+<?php wp_nav_menu( array ('menu' => 'REC', 'container' => '','depth' => 0) ); ?>
+</div>
+	
+	<div class="eight columns">
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php the_content();?>
+		<?php endwhile; ?>
+		<?php endif;?>
+		<?php wp_reset_query(); ?>
+
+<?php $project_menu = get_field('project_portfolio_nav_menu'); ?>
+<?php wp_nav_menu( array ('menu' => $project_menu, 'container' => '','menu_class' => 'tab-nav','depth' => 0) ); ?>
+<?php $project_term = get_field('project_portfolio_category', $term); ?>
+
+<?php $slug = $project_term->slug;?>
+<?php $posts = new WP_Query(array( 
+   'post_type' => 'bgmp',
+   'orderby' => 'meta_value_num',
+	 'meta_key' => 'bgmp_zIndex',
+   'order' => 'DESC',
+	 'bgmp-category' => $slug,
+   'posts_per_page' => -1
+)); ?>
+<?php if ( $posts->have_posts() ) : while ( $posts->have_posts() ) : $posts->the_post(); ?>
+<div class="row project-entry">
+<?php $image = get_field('mini_project_image');
+
+if ($image):?>
+<div class="project-image">
+<p><a href="<?php the_permalink();?>"><img src="<?php the_field('mini_project_image');?>" /></a>
+</div>
+<div class="project-info">
+<h4><?php the_title();?></h4>
+<p class="uppercase"><?php the_field('location');?></p>
+<p><?php the_field('snippet');?></p>
+<p><a href="<?php the_permalink();?>">Read full profile</a></p>
+</div>
+<?php else:?>
+<div class="project-info no-image">
+<h4><?php the_title();?></h4>
+<p class="uppercase"><?php the_field('location');?></p>
+<p><?php the_field('snippet');?></p>
+<p><a href="<?php the_permalink();?>">Read full profile</a></p>
+</div>
+<?php endif;?>
+
+</div>
+
+<?php endwhile; ?>
+<?php endif;?>
+<?php wp_reset_query(); ?>
+		
+	</div>
+	<?php get_template_part('partials/sidebar'); ?>
+	<?php get_template_part('partials/footer'); ?>
--- wp-content/themes/bef/single-bgmp.php
+++ wp-content/themes/bef/single-bgmp.php
@@ -1,34 +1,36 @@
-<?php get_header(); ?>
+<?php get_header('solutions'); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php get_template_part('partials/breadcrumbs'); ?>
+	<div class="four columns">
 	<div class="four columns alpha omega subnav">
-		<?php if( has_term( 'rec', 'bgmp-category' )): ?>
-		<?php wp_nav_menu( array ('menu' => 'REC Projects', 'container' => '','menu_class' => 'tab-nav','depth' => 0) ); ?>
-		<?php else: ?>
-		<?php endif; ?>
+		<h4>Projects and Programs</h4>
+		<?php wp_nav_menu( array ('menu' => 'Carbon Offsets', 'container' => 'nav','menu_class' => '','depth' => 0) ); ?>
 	</div>
+		</div> 
 	<div class="twelve columns">
-		<?php $slideshow = get_field('slideshow');
-		$single_image = get_field('main_image');?>
-		<?php if($slideshow) : ?>
-		<div class="slideshow-wrap">
-			<div id="prev">PREV</div>
-			<div id="next">NEXT</div>
-			<div class="cycle-slideshow" data-cycle-pause-on-hover="true"data-cycle-prev="#prev"
-			data-cycle-next="#next" data-cycle-speed="1000" data-cycle-timeout="5000">
+<p class="back-projects"><a href="javascript:history.back()">RETURN TO ALL PROJECTS</a></p>
+	<?php $slideshow = get_field('slideshow');
+			$single_image = get_field('main_image');?>
+	<?php if($slideshow) : ?>
+	<div class="slideshow-wrap">
+		<div id="prev">PREV</div>
+		<div id="next">NEXT</div>
+		<div class="cycle-slideshow" data-cycle-pause-on-hover="true"data-cycle-prev="#prev"
+        data-cycle-next="#next" data-cycle-speed="1000" data-cycle-timeout="5000">
 			<?php while (the_repeater_field('slideshow')):?>
 			<img src="<?php the_sub_field('image');?>" />
-		<?php endwhile;?>
+			<?php endwhile;?>
+		</div>
 	</div>
-</div>
-<?php elseif ($single_image) :?>
+	<?php elseif ($single_image) :?>
 	<img src="<?php the_field('main_image');?>" />
-<?php else : ?>
-<?php endif;?>	
-<h1><?php the_title();?></h1>
-<p class="uppercase"><?php the_field('location');?></p>
-<?php the_field('project_information');?>
-</div>
-<?php endwhile; ?>
-<?php endif;?>
-
-<?php get_template_part('partials/footer'); ?>
+	<?php else : ?>
+	<?php endif;?>
+	<h1><?php the_title();?></h1>
+	<p class="uppercase"><?php the_field('location');?></p>
+	<?php the_field('project_information');?>
+	</div>
+	<?php endwhile; ?>
+	<?php endif;?>
+	<?php get_template_part('partials/footer'); ?>
