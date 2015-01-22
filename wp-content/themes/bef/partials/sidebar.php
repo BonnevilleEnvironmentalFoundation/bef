885c9216f85ebe1c30316f7d4b50b2eeccef0766
unknown <lars@goodchee.com>
2015-01-22 12:08:11 -0800

    sidebar adjustments

--- wp-content/themes/bef/partials/sidebar.php
+++ wp-content/themes/bef/partials/sidebar.php
@@ -1,20 +1,7 @@
 <div class="four columns sidebar">
 <?php if(get_field('optional_sidebar_content')): ?>
-	<div class="sidebar-callout optional">
 	<?php echo get_field('optional_sidebar_content'); ?>
-</div>
 <?php else: ?>
-<div class="sidebar-callout request-consultation">
-<h3><a href="<?php bloginfo('url');?>/contact-us/consultation-request-form/">schedule a one-on-one consultation</a></h3>
-
-<p>Share your goals, learn more about BEF and explore the benefits of a BEF partnership.</p>
-<a class="button" href="<?php bloginfo('url');?>/contact-us/consultation-request-form/">GO</a>
-</div
-><div class="sidebar-callout newsletter">
-<h3><a href="<?php bloginfo('url');?>/bef-enewsletter/">stay in touch with our ebulletin</a></h3>
-
-<p>Get industry insight, BEF news and latest partner and project stories delivered right in your inbox.</p>
-<a class="button" href="<?php bloginfo('url');?>/bef-enewsletter/">GO</a>
-</div>
+&nbsp;
 <?php endif; ?>
 </div>
